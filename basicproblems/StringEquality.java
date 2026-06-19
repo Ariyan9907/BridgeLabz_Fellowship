681c90216486d5f9f508f7579e4fb32c12b26834
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-19 15:48:26 +0530

    String Equality

---
+++ basicproblems/StringEquality.java
@@ -0,0 +1,14 @@
+package com.src.basicproblems;
+
+import java.util.Scanner;
+
+public class StringEquality {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1=sc.next();
+        String s2=sc.next();
+
+        System.out.println(s1.equals(s2));
+
+    }
+}
