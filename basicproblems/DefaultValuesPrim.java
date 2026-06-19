ddf2e762fbba701c1afaa8f84b8529a9cb049dc6
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-19 15:25:17 +0530

    Default values of primitive datatyepes

---
+++ basicproblems/DefaultValuesPrim.java
@@ -0,0 +1,25 @@
+package com.src.basicproblems;
+
+public class DefaultValuesPrim {
+    byte by;
+    int in;
+    short sh;
+    long ln;
+    float fl;
+    double dl;
+    char ch;
+    boolean bl;
+
+    public static void main(String args[]){
+        DefaultValuesPrim d=new DefaultValuesPrim();
+        System.out.println("Byte:"+d.by);
+        System.out.println("Int:"+d.in);
+        System.out.println("Short:"+d.sh);
+        System.out.println("long:"+d.ln);
+        System.out.println("Float:"+d.fl);
+        System.out.println("Double:"+d.dl);
+        System.out.println("Char:"+d.ch);
+        System.out.println("Boolean:"+d.by);
+
+    }
+}
