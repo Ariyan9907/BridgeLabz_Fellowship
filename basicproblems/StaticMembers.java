2b9eddbb4d2bd6e56f60a3cc14e79f14d1174b8f
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-19 16:25:19 +0530

    [ariyan] Static members progrm

---
+++ basicproblems/StaticMembers.java
@@ -0,0 +1,11 @@
+package com.src.basicproblems;
+
+public class StaticMembers {
+    static String name;
+    static {
+        name="Aryan";
+    }
+    public static void main(String[] args) {
+        System.out.println(name);
+    }
+}
