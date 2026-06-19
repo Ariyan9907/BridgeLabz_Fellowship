5ee97c214e54b57ff324b564bd3ea6c85103fc3d
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-19 17:08:16 +0530

    Sum of Natural number

---
+++ conditionstatements/NaturalSum.java
@@ -0,0 +1,15 @@
+package com.src.conditionstatements;
+
+import java.util.Scanner;
+
+public class NaturalSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
