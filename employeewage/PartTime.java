a2277255f04213a768d07b3be44352ee3f8a3a3f
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-21 01:38:15 +0530

    [Ariyan]: Part time employee rate

---
+++ employeewage/PartTime.java
@@ -0,0 +1,9 @@
+package com.src.employeewage;
+
+public class PartTime {
+    public static void main(String[] args) {
+        int rate=20;
+        int hour=8;
+        System.out.println("Part time employeee wage is:"+rate*hour);
+    }
+}
