fafe55e2ecd3e3b711a74f8c8705ade430629372
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-21 01:20:26 +0530

    [Ariyan]:Welcome Employee

---
+++ employeewage/WelcomeEmployee.java
@@ -0,0 +1,7 @@
+package com.src.employeewage;
+
+public class WelcomeEmployee {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+}
