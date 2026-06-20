07c392e967710cbf8adb64f4d7b7234721a8b750
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-21 01:31:30 +0530

    Calculating employee wage

---
+++ employeewage/EmployeeWage.java
@@ -0,0 +1,9 @@
+package com.src.employeewage;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        int rate=20;
+        int hour=8;
+        System.out.println("Daily employeee wage is:"+rate*hour);
+    }
+}
