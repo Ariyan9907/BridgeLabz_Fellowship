5cd9edf89457154494b022c7a43fd3dcbbf3f9fc
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-21 01:42:54 +0530

    [Ariyan]:Month Wage of employee

---
+++ employeewage/MonthWage.java
@@ -0,0 +1,11 @@
+package com.src.employeewage;
+
+public class MonthWage {
+    public static void main(String[] args) {
+        int rate=20;
+        int hour=8;
+        int days=20;
+
+        System.out.println("Month Wage:"+rate*hour*days);
+    }
+}
