02cd91a89f1a3ae4d0488cf35dc5f0264ab4a5aa
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-21 16:16:08 +0530

    [Ariyan]:updatePosition method added

--- snakegame/SnakeLadder.java
+++ snakegame/SnakeLadder.java
@@ -1,6 +1,10 @@
 package com.src.snakegame;
 
 public class SnakeLadder {
+    static final int noPlay=0;
+    static final int ladder=1;
+    static final int sneak=2;
+
     static int initialPosition(){
         return 0;
     }
@@ -8,6 +12,32 @@ public class SnakeLadder {
     static int rollDice(){
         return (int)(Math.random()*6)+1;
     }
+
+    static int updatePosition(int position,int dice){
+        int oldPos=position;
+        int option=(int)(Math.random()*3);
+
+        if(option==noPlay){
+            return position;
+        }
+        else if(option==ladder){
+
+            if(position>100){
+                position=oldPos;
+            }
+        }
+        else{
+            position-=dice;
+            if (position<0){
+                position= initialPosition();
+            }
+        }
+        return position;
+    }
+
+
+
+
 
 
     public static void main(String[] args) {
