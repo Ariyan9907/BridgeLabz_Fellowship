ec1e4ef62587efd5cba563a0e218089ad724b1f7
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-21 15:01:21 +0530

    [Ariyan] Calculating length

--- geometricity/LineGeometry.java
+++ geometricity/LineGeometry.java
@@ -4,12 +4,18 @@ class GeomtricOp{
     void dispWelcome(){
         System.out.println("Welcome to line comparison computation");
     }
+
+    double calcLength(int x1,int y1,int x2,int y2){
+        return Math.sqrt((Math.pow((x2-x1),2))+(Math.pow((y2-y1),2)));
+    }
 }
+
+
 
 public class LineGeometry {
     public static void main(String[] args) {
         GeomtricOp g = new GeomtricOp();
-        g.dispWelcome();
+        System.out.println(g.calcLength(2,3,6,7));
 
     }
 }
