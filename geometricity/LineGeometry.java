ad2ff564ed707a07d79110d2c9cac0feadfe7983
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-21 15:07:29 +0530

    [Ariyan] Calculating length Equality

--- geometricity/LineGeometry.java
+++ geometricity/LineGeometry.java
@@ -8,6 +8,18 @@ class GeomtricOp{
     double calcLength(int x1,int y1,int x2,int y2){
         return Math.sqrt((Math.pow((x2-x1),2))+(Math.pow((y2-y1),2)));
     }
+
+    void checkEqulatityofTwoLines(){
+        Double l1=calcLength(1,2,4,6);
+        Double l2=calcLength(2,3,5,7);
+
+        if(l1.equals(l2)){
+            System.out.println("Both are equal Lines");
+        }
+        else{
+            System.out.println("Both are not equal lines");
+        }
+    }
 }
 
 
@@ -15,7 +27,7 @@ class GeomtricOp{
 public class LineGeometry {
     public static void main(String[] args) {
         GeomtricOp g = new GeomtricOp();
-        System.out.println(g.calcLength(2,3,6,7));
+        g.checkEqulatityofTwoLines();
 
     }
 }
