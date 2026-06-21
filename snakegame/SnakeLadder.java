0c5f28e095c034a8709aa09b6582ca81ba79e852
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-21 15:58:00 +0530

    [Ariyan]:Initial position added

--- snakegame/SnakeLadder.java
+++ snakegame/SnakeLadder.java
@@ -1,7 +1,11 @@
 package com.src.snakegame;
 
 public class SnakeLadder {
+    static int initialPosition(){
+        return 0;
+    }
     public static void main(String[] args) {
-
+        int position=initialPosition();
+        System.out.println(position);
     }
 }
