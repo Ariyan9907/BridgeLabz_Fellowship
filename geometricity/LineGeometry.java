ecf439f510e58c8019eb1dc25640860c56dec8d3
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-21 14:53:43 +0530

    Welcome Display

--- geometricity/LineGeometry.java
+++ geometricity/LineGeometry.java
@@ -1,7 +1,15 @@
 package com.src.geometricity;
 
+class GeomtricOp{
+    void dispWelcome(){
+        System.out.println("Welcome to line comparison computation");
+    }
+}
+
 public class LineGeometry {
     public static void main(String[] args) {
+        GeomtricOp g = new GeomtricOp();
+        g.dispWelcome();
 
     }
 }
