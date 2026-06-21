45308e580a884b561f57983eb5c678a9e7c53a55
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-21 15:59:53 +0530

    [Ariyan]:Dice method added

--- snakegame/SnakeLadder.java
+++ snakegame/SnakeLadder.java
@@ -4,8 +4,16 @@ public class SnakeLadder {
     static int initialPosition(){
         return 0;
     }
+
+    static int rollDice(){
+        return (int)(Math.random()*6)+1;
+    }
+
+
     public static void main(String[] args) {
         int position=initialPosition();
-        System.out.println(position);
+        int dice=rollDice();
+        System.out.println(dice);
+
     }
 }
